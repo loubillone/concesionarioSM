2974eab99f5c5d6960bea1d169bbbb624e7dc667
Lourdes Billone <lou.billone@gmail.com>
2024-07-31 14:24:37 -0300

    armo tabla y pruebo petición de lista de usuarios

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,10 +1,62 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Navegador from "../components/Navegador";
+import Table from "react-bootstrap/Table";
+import testApi from "../api/testApi";
 
 const Admin = () => {
+  const [cargarUsuarios, setCargarUsuarios] = useState([]);
+  useEffect(() => {
+    listaUsuariosBack();
+  }, []);
+  const listaUsuariosBack = async () => {
+    try {
+      const resp = await testApi.get("/admin/listaUsuarios");
+
+      setCargarUsuarios(resp.data.listaUsuarios);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <div>
       <Navegador />
+
+      <div className="container my-3">
+        <h2>Administrador</h2>
+        <div className="row my-4">
+          <div className="col">
+            <Table striped bordered hover>
+              <thead>
+                <tr>
+                  <th>ID</th>
+                  <th>Nombre</th>
+                  <th>Email</th>
+                  <th>Rol</th>
+                  <th>Eliminar</th>
+                </tr>
+              </thead>
+              <tbody>
+                {cargarUsuarios.map((usuario) => {
+                  return (
+                    <tr key={usuario._id}>
+                      <td>{usuario._id}</td>
+                      <td>{usuario.name}</td>
+                      <td>{usuario.email}</td>
+                      <td>{usuario.rol}</td>
+                      <td>
+                        <button class="btn btn-danger">
+                          {" "}
+                          <i class="fa fa-trash-o" aria-hidden="true"></i>
+                        </button>
+                      </td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </Table>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
